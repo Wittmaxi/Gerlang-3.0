b15dd471f8ed8b1bb963afd30484c7f4ae671fef
wittmaxi@outlook.de <wittmaxi@outlook.de>
2018-02-28 21:31:12 +0100

    improvements of the "scope class"

--- src/helpers/parserutil/scope.h
+++ src/helpers/parserutil/scope.h
@@ -1,4 +1,23 @@
 class scope {
-		bool anynome;
-		std::vector <std::string> code;
+	void addVars (std::vector<std::tuple<std::string, std::string>> vars) { 
+		//takes in a vector of <variablename, typedenominator>
+		for (auto i : vars) {
+			variables.push_back (i);
+		}
+	}
+	void addVar (std::tuple <std::string, std::string> var) {
+		variables.push_back (i);
+	}
+	bool varExists (std::string name) { //does a specific variable already exist?
+		for (auto i : variables) {
+			if (std::get <0> (i) == name) {
+				return true;
+			}
+		} 
+		return false;
+	}
+	
+	std::vector<std::tuple <std::string, std::string>> variables;
+	bool anynome;
+	std::vector <std::string> code;
 };
